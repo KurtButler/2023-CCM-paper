2cce51811d5eb6a604847731d7312cd9f4a599b7
Kurt Butler <62613188+newyorkfishmarket@users.noreply.github.com>
2023-05-05 12:23:25 -0400

    Create main.m

---
+++ main.m
@@ -0,0 +1,23 @@
+%% INTRO TEXT
+
+
+%% Set up the path
+addpath('./Functions')
+addpath('./Results')
+addpath('./Scripts')
+
+
+%% Figure 8
+
+Figure_8
+
+
+
+%% Table I
+
+System_L1
+System_L2
+System_B1
+System_B2
+System_K1
+System_K2
