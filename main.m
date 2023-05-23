dffbec412fcbbcd0202fbf15546e9e961133d4c6
Kurt Butler <62613188+newyorkfishmarket@users.noreply.github.com>
2023-05-23 16:43:04 -0400

    Added comments to main.m

--- main.m
+++ main.m
@@ -1,4 +1,9 @@
-%% INTRO TEXT
+%% Matlab main file
+% Title:   On Causal Discovery with Convergent Cross Mapping
+% Author:   Kurt Butler
+% Date:     May 23, 2022
+% Description:  Reproduces all listed Figures and Tables from the paper, 
+% and configures the MATLAB path to spit out files.
 
 
 %% Set up the path
@@ -7,14 +12,21 @@ addpath('./Results')
 addpath('./Scripts')
 
 
-%% Figure 8
+%% Figures
+% Figure 4: Basic demonstration of CCM.
 
+% Figure 5: Comparison of the surrogate data test for a deterministic signal vs a random one.
+
+% Figure 6: Comparison of distance, covariance, correlation and recurrence matrices.
+
+% Figure 7: Demonstration of the recurrence principle with a pair of sinusoids.
+
+% Figure 8: Histogram of CCM convergence coefficients for a Rossler-Lorenz system.
 Figure_8
 
 
-
 %% Table I
-
+% This will return the table data as text in the command window.
 System_L1
 System_L2
 System_B1
