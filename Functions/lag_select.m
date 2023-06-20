2708d34fd1fa1ec201d47c1c4f3ed3a1df8f8350
Kurt Butler <62613188+KurtButler@users.noreply.github.com>
2023-06-20 13:17:05 -0400

    Delete Functions directory

--- Functions/lag_select.m
+++
@@ -1,8 +0,0 @@
-function tau = lag_select(x,theta)
-% Select tau for SSR using the autocorrelation function
-ACF = autocorr(x);
-if all(ACF>=theta)
-    ACF = autocorr(x,min(numel(x)-1, 100));
-end
-tau = find(ACF<theta,1) - 1;
-end
